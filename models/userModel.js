523f8838470d5e225b72806f3c30df20b7f74bdf
DESKTOP-KC0AM8L\Ryan <ryanmlerner@gmail.com>
2024-04-04 15:39:09 -0600

    add login method, jwt functionality

--- models/userModel.js
+++ models/userModel.js
@@ -36,13 +36,38 @@ userSchema.statics.signup = async function (email, password) {
   const exists = await this.findOne({ email })
 
   if (exists) {
-    throw Error(' Email already in use')
+    throw Error('Email already in use')
   }
 
   const salt = await bcrypt.genSalt(10)
   const hash = await bcrypt.hash(password, salt)
 
   const user = await this.create({ email, password: hash })
+
+  return user
+}
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+
+  // validation
+  if (!email || !password) {
+    throw Error('All fields must be filled')
+  }
+
+  const user = await this.findOne({ email })
+
+  if (!user) {
+    throw Error('Incorrect email')
+    // throw Error('Invalid login credentials')
+  }
+
+  const match = await bcrypt.compare(password, user.password)
+
+  if (!match) {
+    throw Error('Incorrect password')
+    // throw Error('Invalid login credentials')
+  }
 
   return user
 }
